77ca0376eae22b2590bee821d785719bb520c1cb
vitorita godoy <133662768+vivick12@users.noreply.github.com>
2024-04-17 16:20:36 -0300

    Add files via upload

---
+++ primeiro projeto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            /*  lição 1*/
+
+            /* int dinheiro = 35;
+             int horas = 8;
+
+             int salario = dinheiro * horas;
+
+             Console.WriteLine("Seu salário é :" + salario);*/
+
+            /*  lição 2*/
+
+            /*  int numero1 = 27;
+              int numero2 = 16;
+
+              int subtracao = numero1 - numero2;
+
+              Console.WriteLine("Ela tem: " + subtracao + " Bombons");*/
+
+            /*  lição 3*/
+
+            /* int numero01 = 12;
+             int numero02 = 10;
+
+             Console.WriteLine("Seus Numeros são: " + numero01 + " e " + numero02);
+
+             int soma = numero01 + numero02;
+
+             Console.WriteLine("Seu total é: " + soma);*/
+
+
+            /*  lição 4*/
+
+            /*int numero1 = 300;
+            int numero2 = 20;
+
+
+            int divisao = numero1 / numero2;
+
+
+            Console.WriteLine("essa é a quantidade de caixas utilizadas: " + divisao);*/
+
+
+            /*  lição 5*/
+
+            double precoInstalacao = 300.00;
+            double precoLimpeza = 150.00;
+            double precoManutencao = 499.99;
+
+
+            double descontoNovoCliente = 50.00;
+
+
+            double totalServicos = precoInstalacao + precoLimpeza + precoManutencao;
+
+
+            double totalAPagar = totalServicos - descontoNovoCliente;
+
+
+            Console.WriteLine("O total a ser pago pela empresa TERMAL é: R$" + totalAPagar);
+
+
+
+
+        }
+
+    }
+
+
+
+
+}
+
